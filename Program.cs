4afb86142a98d019663fedc01eb9d99fc1591b51
Grasenpi <122781397+Grasenpi@users.noreply.github.com>
2024-02-27 01:05:33 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rabota1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x:");
+            int x = int.Parse(Console.ReadLine());
+            int y = 5 * x + 20;
+            Console.WriteLine($"Результат выражения y = {y}");
+        }
+    }
+}
